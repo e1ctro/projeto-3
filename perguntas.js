6a72e6a900e6889287760aef6b0ebfd320867bf0
e1ctro <00001105890521sp@al.educacao.sp.gov.br>
2024-11-25 09:51:18 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'abel abel',
+    'quem ganhou o 1 nobel de fisica',
+    'O primeiro ganhador do Nobel de Física foi Wilhelm Conrad Röntgen em 1901'
+)
+
+criaCartao(
+    'amostradinho do ano',
+    'quem é a pessoa que mais odeia o kaue',
+    'prof lais'
+)
+
+criaCartao(
+    'qual',
+    'qual das perguntas é a mais inultil',
+    'voce sabe da piada do poney?'
+)
+
+criaCartao(
+    'Lingua inglesaどうやって読んでいますか？',
+    'ブラジルで一番大きなチームはどこですか?',
+    'コリント人への手紙'
+)
